a6f4ca0c62adbd7f71f48ca9d620d3bad7d2d922
Amir <nightdvlpr@gmail.com>
2020-10-15 17:51:17 +0330

    Const variables changed

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ window.onload = () => {
   console.log('________________________ Mathematical Simulation ________________________');
 
   // data members
-  const lambda = .2; //λ
+  const lambda = 0.2; //λ
   const theta = 2 * Math.PI; //θ
   const vf = 2.55;
   const alpha = 14.82;
-  const k = Math.pow(10, 10);
+  const k = 10;// Math.pow(10, 10);
   const h = 1;
-  const m = 32.55;
+  const m = 35.21;
   let v;
 
   v = vf * (1 + Math.pow(alpha * k, 2));
@@ -17,9 +17,12 @@ window.onload = () => {
   // let H = (Math.pow(lambda, 2) * Math.pow(v, 2) * Math.pow(k, 2));
 
   let E_zero = (Math.pow(h, 2) * Math.pow(k, 2)) / (2 * m);
-  let E_plus = E_zero + (Math.sqrt((Math.pow(v, 2) * Math.pow(k, 2) + Math.pow(lambda, 2) * Math.pow(k, 6) * Math.pow(Math.cos(3 * theta), 2))));
+
+  let E_plus = E_zero + Math.sqrt((Math.pow(v, 2) * Math.pow(k, 2) + Math.pow(lambda, 2) * Math.pow(k, 6) * Math.pow(Math.cos(3 * theta), 2)));
+
   let E_minus = E_zero - (Math.sqrt((Math.pow(v, 2) * Math.pow(k, 2) + Math.pow(lambda, 2) * Math.pow(k, 6) * Math.pow(Math.cos(3 * theta), 2))));
 
+  prt("v => " + v);
   prt("E0 => " + E_zero);
   prt("E- => " + E_plus);
   prt("E+ => "+E_minus);
