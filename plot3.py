b65e126dade5d88f83516aa81464e295408b2d78
nightdvlpr <nightdvlpr@gmail.com>
2020-10-25 20:45:26 +0330

    Plot3 finish

--- plot3.py
+++ plot3.py
@@ -7,39 +7,33 @@ plt.title('FIG. 3.')
 
 # X
 plt.xlabel(r'$\mathcal{\omega (eV)} $', fontsize=24)
-plt.xticks([0.35, .40, .45, .50, .60, .65, .70])
+plt.xticks([-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5])
 
 # Y
-plt.ylabel(r'$\sigma (\omega) in-unit-of (2 \pi e^2/h)$', fontsize=24)
-plt.yticks([0, .1, .2, .3, .4])
+plt.ylabel('DOS', fontsize=24)
+plt.yticks([0, .2, .4, .6])
 
 
 # axis
-plt.axis([0.35, .70, 0, .4])
+plt.axis([-0.5, 0.5, 0, .7])
 
-x1 = [.37, .38, .39, .40, .70]
-y1 = [0, .008, .011, .06, .06]
+x1 = [-.5, 0, .5]
+y1 = [.65, 0, .65]
 
-x2 = [.37, .38, .39, .40, .45, .50, .55, .60, .65, .70]
-y2 = [0, .009, .011, .07, .08, .081, .083, .085, .087, .1]
+x2 = [-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+y2 = [.3, .29, .27, .22, .13, 0, .13, .22, .27, .29, .3]
 
-x3 = [.37, .38, .39, .40, .45, .50, .55, .60, .65, .70]
-y3 = [0, .008, .011, .09, .1, .11, .12, .14, .15, .18]
-
-x4 = [.37, .38, .39, .40, .45, .50, .55, .60, .65, .70]
-y4 = [0, .008, .011, .12, .15, .19, .22, .26, .29, .35]
+x3 = [-.5, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+y3 = [.18, .2, .2, .2, .13, 0, .13, .2, .2, .2, .18]
 
 plt.plot(x1, y1, marker='x', linestyle='solid',
-         color='#000', label='Lambda=0', markersize=4)
+         color='#000000', label=r'$\lambda=0$', markersize=4)
 
 plt.plot(x2, y2, marker='X', linestyle='-',
-         color='#ff0000', label='Lambda=0.05',markersize=5)
+         color='#ff0000', label=r'$\lambda=0.1$', markersize=5)
 
-plt.plot(x3, y3, marker='D', linestyle='-.',
-         color='#00ff00', label='Lambda=0.1', markersize=6)
-
-plt.plot(x4, y4, marker='P', linestyle='--',
-         color='#0000ff', label='3eV', markersize=7) 
+plt.plot(x3, y3, marker='D', linestyle='dotted',
+         color='#0000ff', label=r'$\lambda=0.2$', markersize=6)
 
 # show
 plt.legend()
