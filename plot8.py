d58dbaa580143db55bee20b09c5cdae2b343f711
nightdvlpr <nightdvlpr@gmail.com>
2020-11-15 23:25:38 +0330

    Change file

--- plot8.py
+++ plot8.py
@@ -8,6 +8,7 @@ plt.title("(c)")
 plt.xlabel(r"hw(me V)")
 plt.xticks([0, 20, 40, 50])
 
+
 # Y
 plt.ylabel(r"")
 plt.yticks([0, 5, 10, 15, 20])
@@ -25,10 +26,9 @@ y2 = np.array([15,15, 18,10,22,9,          8.5, 7, 6.5, 6.1, 5.9, 5.5, 4.4, 2   
 x3 = np.array([0, 0, 17, 17,19,20,20,   20.2, 21.5, 22.5, 23, 24, 25, 28, 50])
 y3 = np.array([0, 1, 0.5, 4.2,5,6,16,   5.9, 3.9, 3.4, 3.1, 2.5, 1.9, 1.5, 0.4])
 
-plt.plot(x2, y2, marker="", linestyle="-.", color="#0000ff", label="", linewidth=2.1)
-plt.plot(x1, y1, marker="", linestyle="-", color="#000", label="", linewidth=2)
-
+plt.plot(x2, y2, marker="", linestyle="-.", color="#0000ff", label="", linewidth=1.1)
 plt.plot(x3, y3, marker="", linestyle="--", color="#ff0000", label="", linewidth=2.1)
+plt.plot(x1, y1, marker="", linestyle="-", color="#000", label="", linewidth=2)
 
 # show
 plt.legend()
