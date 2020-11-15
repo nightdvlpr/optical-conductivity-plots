729cc734a0c77e15e90d405222c8fefb2c5629b2
nightdvlpr <nightdvlpr@gmail.com>
2020-11-16 01:25:46 +0330

    Add plot11

---
+++ plot10.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# title
+plt.title("(c)")
+
+# X
+plt.xlabel(r"hw(me V)")
+plt.xticks([0, 15, 30, 45, 50])
+
+# Y
+plt.ylabel(r"D(w)")
+plt.yticks([0, 15, 30, 45])
+
+# axis
+plt.axis([0, 50, 0, 45])
+
+x1 = np.array([0,15.5, 15.5, 16, 17,18, 20, 20.1])
+y1 = np.array([0.2, 0.2,9, 10.2,10.8,11.5, 17, 43])
+x2 = np.array([50, 40, 30, 25, 22, 21, 20.5, 20, 20])
+y2 = np.array([3, 3.5, 4, 5, 5.5, 6, 6.5, 8, 43])
+
+x3 = np.array([0.2, 0.2, 5, 15, 16, 17,18, 20, 20.1])
+y3 = np.array([0, 9, 9.1, 9.6, 10.2,10.8,11.5, 17, 43])
+x4 = np.array([50, 40, 30, 25, 22, 21, 20.5, 20, 20])
+y4= np.array([3, 3.5, 4, 5, 5.5, 6, 6.5, 8, 43])
+
+x5 = np.array([0, 2,2, 5, 15, 16, 17,18, 20, 20.1])
+y5 = np.array([0.2, 0.2,9, 9.1, 9.6, 10.2,10.8,11.5, 17, 43])
+x6 = np.array([50, 40, 30, 25, 22, 21, 20.5, 20, 20])
+y6= np.array([3, 3.5, 4, 5, 5.5, 6, 6.5, 8, 43])
+
+plt.plot(x1, y1, marker="", linestyle="dotted", color="#ff0000", label="", linewidth=3)
+plt.plot(x2, y2, marker="", linestyle="dotted", color="#ff0000", label="mu= 8 me V", linewidth=3)
+plt.plot(x3, y3, marker="", linestyle="-", color="#000000", label="")
+plt.plot(x4, y4, marker="", linestyle="-", color="#000000", label="mu = 0")
+plt.plot(x5, y5, marker="", linestyle="--", color="#0000ff", label="", linewidth=2)
+plt.plot(x6, y6, marker="", linestyle="--", color="#0000ff", label="mu= 1 me V", linewidth=1)
+
+# show
+plt.legend()
+plt.show()
---
+++ plot11.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# title
+plt.title("(c)")
+
+# X
+plt.xlabel(r"hw(me V)")
+plt.xticks([0, 20, 40, 50])
+
+
+# Y
+plt.ylabel(r"")
+plt.yticks([0, 5, 10, 15, 20])
+
+# axis
+plt.axis([0, 50, 0, 20])
+
+x1 = np.array(
+    [0.2, 0.2, 2.9, 17, 18, 18.5, 19, 20, 20, 20.2, 21.5, 22.5, 23, 24, 25, 28, 50]
+)
+y1 = np.array(
+    [0, 2.1, 2.1, 3.4, 3.8, 4, 4.2, 5, 15, 5, 3.5, 3, 2.7, 2.2, 1.8, 1.4, 0.4]
+)
+
+x2 = np.array([0, 0, 17, 17, 19, 20, 20, 20.2, 21.5, 22.5, 23, 24, 25, 28, 50])
+y2 = np.array([0, 1, 0.5, 4.2, 5, 6, 16, 5.9, 3.9, 3.4, 3.1, 2.5, 1.9, 1.5, 0.4])
+
+x3 = np.array(
+    [0, 2, 2, 2.9, 17, 18, 18.5, 19, 20, 20, 20.2, 21.5, 22.5, 23, 24, 25, 28, 50]
+)
+y3 = np.array(
+    [0, 0, 2.1, 2.1, 3.4, 3.8, 4, 4.2, 5, 15, 5, 3.5, 3, 2.7, 2.2, 1.8, 1.4, 0.4]
+)
+
+plt.plot(x3, y3, marker="", linestyle="--", color="#0000ff", label="", linewidth=2.1)
+plt.plot(x1, y1, marker="", linestyle="-", color="#000", label="", linewidth=1.5)
+plt.plot(x2, y2, marker="", linestyle="dotted", color="#ff0000", label="", linewidth=3)
+
+# show
+plt.legend()
+plt.show()
